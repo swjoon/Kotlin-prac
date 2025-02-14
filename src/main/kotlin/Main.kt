d64ee7daae5bb7ee9fdff685741da17dd18b4837
swjoon <jetkid1446@gmail.com>
2025-02-14 17:20:07 +0900

    fix : 주석삭제

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,7 @@ import org.SBB.controller.AppController
 import org.SBB.repository.AppRepository
 import org.SBB.service.AppService
 import org.SBB.util.UrlUtil
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
     println("== 명언 앱 ==")
     switch(
