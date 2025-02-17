99a4d15903aa881e37abcfd82748ddd676508b52
swjoon <jetkid1446@gmail.com>
2025-02-17 12:44:11 +0900

    refactor : singleton 적용

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,30 @@
 package org.SBB
 
-import org.SBB.controller.AppController
-import org.SBB.repository.AppRepository
-import org.SBB.service.AppService
-import org.SBB.util.UrlUtil
+import org.SBB.domain.controller.AppController
+import org.SBB.global.config.SingletonManager
+import org.SBB.global.util.UrlUtil
 
 fun main() {
     println("== 명언 앱 ==")
-    switch(
-        appController = AppController(AppService(appRepository = AppRepository())),
-        urlUtil = UrlUtil(),
-    )
+    switch(SingletonManager.controller)
 }
 
-fun switch(appController: AppController, urlUtil: UrlUtil){
-    while(true){
+fun switch(appController: AppController) {
+    while (true) {
         print("명령) ")
         val input = readlnOrNull()!!.trim();
-        val url = urlUtil.getUrl(input);
+        val url = UrlUtil.getUrl(input);
 
-        when(url.action){
+        when (url.action) {
             "등록" -> appController.SaveController();
-            "목록" -> appController.ListController();
-            "삭제" -> appController.DeleteController(url.id);
-            "수정" -> appController.ModifyController(url.id);
-            "종료" -> return;
+            "목록" -> appController.ListController(url);
+            "삭제" -> appController.DeleteController(url);
+            "수정" -> appController.ModifyController(url);
+            "빌드" -> appController.BuildController()
+            "종료" -> {
+                appController.BuildController()
+                return
+            }
         }
-
     }
 }
