5c3f91987735a72d8f071343f05856fded94c5d0
swjoon <jetkid1446@gmail.com>
2025-02-17 12:47:25 +0900

    add : 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 group = "org.SBB"
@@ -10,6 +11,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     testImplementation(kotlin("test"))
 }
 
